e05a14e88f0a7461c8bd3e86ca90acadf8c4eb2e
Piotr Wolski <piotr.wolski@cyberuslabs.com>
2023-01-23 21:37:21 +0100

    simplify GPS prompt logic.

--- src/sdk/geoProvider/html5GeoProvider.ts
+++ src/sdk/geoProvider/html5GeoProvider.ts
@@ -1,5 +1,5 @@
-import { Geolocation } from './geo';
-import { GeoProvider } from './geoProvider';
+import {Geolocation} from './geo';
+import {GeoProvider} from './geoProvider';
 
 
 /**
@@ -11,80 +11,87 @@ import { GeoProvider } from './geoProvider';
  * @implements {GeoProvider}
  */
 export class HTML5GeoProvider implements GeoProvider {
-  private readonly _enableHighAccuracy: boolean;
-  private _navigator: Navigator;
-  private readonly _numOfTriesBeforeGpsActivates: number = 2;
-  private readonly _onPermissionDialog?: Function;
-  private readonly defaultMessage = "Cyberus Key widget would like to access your location" +
-      " to use geolocation tracking to assert successful authentication."
-  /**
-   * Creates an instance of HTML5GeoProvider.
-   *
-   * @param {boolean} [enableHighAccuracy=false]  Forces high accuracy of the geolocation. It may take longer.
-   * @param {Navigator} [navigator=window.navigator]
-   * @param numOfTriesBeforeGpsActivates The GPS localization will be used only after n unsuccessful tries.
-   * By unsuccessful try we define the number of times the authentication resulted in session not found error.
-   * @param onPermissionDialog Leave an implementation of the additional information dialog to appear before site
-   * asks for localization permission for the caller to handle. It takes a function with default message as parameter
-   * @memberof HTML5GeoProvider
-   */
-  constructor(enableHighAccuracy = false, onPermissionDialog:Function = null, numOfTriesBeforeGpsActivates: number = 2, navigator: Navigator = window.navigator) {
-    this._enableHighAccuracy = enableHighAccuracy;
-    this._navigator = navigator;
-    this._numOfTriesBeforeGpsActivates = numOfTriesBeforeGpsActivates;
-    this._onPermissionDialog = onPermissionDialog;
+    private readonly _enableHighAccuracy: boolean;
+    private _navigator: Navigator;
+    private readonly _numOfTriesBeforeGpsActivates: number = 2;
+    private readonly _onPermissionDialog?: Function;
+    private readonly defaultMessage = "Cyberus Key widget would like to access your location" +
+        " to use geolocation tracking to assert successful authentication."
 
-  }
+    /**
+     * Creates an instance of HTML5GeoProvider.
+     *
+     * @param {boolean} [enableHighAccuracy=false]  Forces high accuracy of the geolocation. It may take longer.
+     * @param {Navigator} [navigator=window.navigator]
+     * @param numOfTriesBeforeGpsActivates The GPS localization will be used only after n unsuccessful tries.
+     * By unsuccessful try we define the number of times the authentication resulted in session not found error.
+     * @param onPermissionDialog Leave an implementation of the additional information dialog to appear before site
+     * asks for localization permission for the caller to handle. It takes a function with default message as parameter
+     * @memberof HTML5GeoProvider
+     */
+    constructor(enableHighAccuracy = false, onPermissionDialog: Function = null, numOfTriesBeforeGpsActivates: number = 2, navigator: Navigator = window.navigator) {
+        this._enableHighAccuracy = enableHighAccuracy;
+        this._navigator = navigator;
+        this._numOfTriesBeforeGpsActivates = numOfTriesBeforeGpsActivates;
+        this._onPermissionDialog = onPermissionDialog;
 
-  /**
-   * Gets a geolocalization measurement.
-   *
-   * @returns {Promise<Geolocation>} Geolocalization measurement.
-   * @memberof HTML5GeoProvider
-   */
-  async getGeo(): Promise<Geolocation> {
-    let result = null;
-
-
-
-    let data = sessionStorage.getItem("auth_counter");
-    if (data ==null)
-      return null;
-
-    let value = parseInt(data, 10)
-
-    if(isNaN(value) || value < this._numOfTriesBeforeGpsActivates)
-      return null;
-
-    try {
-      const permissionDialog = this._onPermissionDialog;
-      const defaultMessage = this.defaultMessage;
-
-      navigator.permissions && navigator.permissions.query({name: 'geolocation'})
-          .then(function(PermissionStatus) {
-            if (PermissionStatus.state == 'prompt') {
-              if(permissionDialog)
-                permissionDialog(defaultMessage)
-              else
-                alert(defaultMessage);
-            }
-          })
-
-
-      result = await this._getGeo(this._enableHighAccuracy);
-    } catch {
-      // E.g. user didn't agree on geolicalization.
-      return null;
     }
 
-    const { coords } = result;
+    /**
+     * Gets a geolocalization measurement.
+     *
+     * @returns {Promise<Geolocation>} Geolocalization measurement.
+     * @memberof HTML5GeoProvider
+     */
+    async getGeo(): Promise<Geolocation> {
 
-    return new Geolocation(coords.latitude, coords.longitude, coords.accuracy);
-  }
+        const permissionStatus = await navigator.permissions.query({
+            name: 'geolocation'
+        });
 
-  _getGeo(enableHighAccuracy: boolean): Promise<Position> {
-    return new Promise((resolve, reject) => {
-      this._navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy })
-    });
-  }
+        try {
+            if (permissionStatus.state == "granted")
+                return await this._getGeo(this._enableHighAccuracy);
+
+            if (permissionStatus.state == "denied")
+                return null;
+
+            if (permissionStatus.state == "prompt") {
+                if (!this._doIShowCustomPrompt())
+                    return null;
+
+                if (this._onPermissionDialog)
+                    this._onPermissionDialog(this.defaultMessage)
+                else
+                    alert(this.defaultMessage);
+
+                return await this._getGeo(this._enableHighAccuracy);
+
+            }
+        } catch (e) {
+            return null;
+        }
+
+    }
+
+    _doIShowCustomPrompt() {
+        let data = sessionStorage.getItem("auth_counter");
+        if (data == null)
+            return false;
+
+        let value = parseInt(data, 10)
+
+        return !isNaN(value) && value >= this._numOfTriesBeforeGpsActivates;
+
+
+    }
+
+    async _getGeo(enableHighAccuracy: boolean): Promise<Geolocation> {
+        const {coords} = await new Promise((resolve, reject) => {
+            this._navigator.geolocation.getCurrentPosition(resolve, reject, {enableHighAccuracy})
+        });
+
+        return new Geolocation(coords.latitude, coords.longitude, coords.accuracy);
+
+    }
 }
